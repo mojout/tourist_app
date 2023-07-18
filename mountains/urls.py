a47c00d3fb961686df8775c738e2ed09711e78f1
Dmitry Zaharov <98744363+Dimose75@users.noreply.github.com>
2023-07-18 09:09:01 +0400

    add logging, optimization query, swagger enabled

--- mountains/urls.py
+++ mountains/urls.py
@@ -1,10 +1,14 @@
 from rest_framework import routers
 from .views import Mountainsviewset
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 router = routers.DefaultRouter()
 router.register(r'submitdata', Mountainsviewset, basename='submitdata')
 
 urlpatterns = [
     path('', include(router.urls)),
+
 ]
+
+urlpatterns += doc_urls
--- mountains/views.py
+++ mountains/views.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .serializers import MountainSerializer
@@ -6,8 +8,16 @@ from .models import Mountain
 
 
 class Mountainsviewset(viewsets.ModelViewSet):
-    queryset = Mountain.objects.all()
+    queryset = Mountain.objects.all().prefetch_related('user', 'coords', 'level', 'images')
     serializer_class = MountainSerializer
     http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
 
 
+@api_view(['get'])
+def get_data(request, pk):
+    try:
+        mountain = Mountain.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        return Response(status=401)
+    serializer = MountainSerializer(mountain)
+    return Response(serializer.data)
