eef0e3b4e0c9f430a41ce6a800c33eae986904d1
Dmitry Zaharov <mojout@yandex.ru>
2023-07-26 19:25:25 +0400

    Update settings.py

--- tourist_app_drf/settings.py
+++ tourist_app_drf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-6p!#!q5h*9b^51ujer97py13_vv_#si_n2o2qeizh9d^auyay4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
